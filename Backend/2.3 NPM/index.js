5bce24f0fdd447194061088ab0da902a2a028861
sgrazys <s.grazys@gmail.com>
2023-12-12 21:50:27 +0200

    some node

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from 'sillyname';
+import superheroes from 'superheroes';
+
+var sillyName = generateName();
+
+superheroes.all;
+let superheroName = superheroes.random();
+
+// console.log(`My name is ${sillyName}.`);
+console.log(`My name is ${superheroName}`);
